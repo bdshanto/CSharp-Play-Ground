096d1b4019569c377804cb563cc3bba03d0fb0b0
HasiBul Islam ShanTo <bdshanto@hotmail.com>
2022-08-15 11:19:06 +0600

    Init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace C_9GenericsApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var stack = new Stack();
+      stack.Add(1.1);
+      stack.Add(2.2);
+      stack.Add(3.8);
+
+
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
