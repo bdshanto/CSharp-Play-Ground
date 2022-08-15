20cca5e4e6f8689d4ceb590888252a0241fba6c5
HasiBul Islam ShanTo <bdshanto@hotmail.com>
2022-08-15 11:42:24 +0600

    stack added

---
+++ CSharpPlayGround/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpPlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Add(1.1);
+            stack.Add(2.2);
+            stack.Add(3.8);
+
+            double count = 0.0;
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}\n");
+                count += item; 
+            }
+            Console.WriteLine($"Sum: {count}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpPlayGround/Stack.cs
@@ -0,0 +1,17 @@
+namespace CSharpPlayGround
+{
+    public class Stack
+    {
+        private double[] Numbers;
+        private int CurrentIndex = -1;
+        public Stack() => Numbers = new double[10];
+        public int Count => CurrentIndex + 1;
+
+
+        public void Add(double item) => Numbers[++CurrentIndex] = item;
+        public double Pop() => Numbers[CurrentIndex--];
+
+
+
+    }
+}
